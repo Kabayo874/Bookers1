8d7a0b606e4428720f120662321a8f74259eb997
EC2 Default User <ec2-user@ip-172-31-5-155.ap-northeast-1.compute.internal>
2025-02-17 10:33:53 +0000

    [Finish]new_book_form, but not test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,26 @@ class BooksController < ApplicationController
   def index
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to 'index_book_path'
+  end
+
   def show
   end
 
   def edit
   end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,7 @@ Rails.application.routes.draw do
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+#  resourceで指定しているが欲しいページが表示されない
+# rails routesで確認。books/indexというルートは無かった
